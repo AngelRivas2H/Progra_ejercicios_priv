9e70744dac69ff8b04dbd525445dc1b59040c3e3
Angel Rivas <angelrivas@localhost.localdomain>
2019-04-30 23:25:58 -0500

    Cambios funcionando al 100.

--- examen_progra2_examen/examen.cpp
+++ examen_progra2_examen/examen.cpp
@@ -25,11 +25,17 @@ int main(){
   string line;
   int lineCount;
   char lineaArchivo[20], lineaBusqueda[20];
+  char acentoA = 'á';
+  char acentoE = 'é';
+  char acentoI = 'í';
+  char acentoO = 'ó';
+  char acentoU = 'ú';
   archivo.open("TipoB.txt");
   if (archivo.is_open()) { // se verifica que el archivo este abierto
     while (getline(archivo, line)) { //mientras haya una linea en al archivo se sigue el bucle
-      char convertido[line.size()+1];
-      strcpy(convertido, line.c_str());
+      char convertido[line.size()+1]; // creacion del char en funcion del tamaño del string
+      strcpy(convertido, line.c_str()); //conversion de string a char
+      // Reinicio de las variables
       contCapital = 0;
       contLowerA = 0;
       contWarn = 0;
@@ -37,31 +43,30 @@ int main(){
       contSpaces = 0;
       lineCount++;
       for (i = 0; i <= sizeof(convertido)-1; i++) {
-        // cout << convertido[i];
-        // cout << "ascii test: " << "opci\u00E1n" << endl;
-        // cout << "ascii test: " << "opci\u00E9n" << endl;
-        // cout << "ascii test: " << "opci\u00EDn" << endl;
-        // cout << "ascii test: " << "opci\u00F3n" << endl;
-        // cout << "ascii test: " << "opci\u00FAn" << endl;
-        if (convertido[i] == 'a') {
+        // cout << convertido[i]; //impresion del archivo caracter por caracter
+        // cout << "ascii test: " << "opci\u00E1n" << endl; // á
+        // cout << "ascii test: " << "opci\u00E9n" << endl; // é
+        // cout << "ascii test: " << "opci\u00EDn" << endl; // í
+        // cout << "ascii test: " << "opci\u00F3n" << endl; // ó
+        // cout << "ascii test: " << "opci\u00FAn" << endl; // ú
+        if (convertido[i] == 'a') { //conteo de letas a
           contLowerA++;
         }
-        if(isalpha(convertido[i]) || (convertido[i] == '\u00E1' || convertido[i] == '\u00E9' || convertido[i] == '\u00ED' || convertido[i] == '\u00F3' || convertido[i] == '\u00FA')){
+        if(isalpha(convertido[i]) || convertido[i] == acentoA || convertido[i] == acentoE || convertido[i] == acentoI || convertido[i] == acentoO || convertido[i] == acentoU) {
           contLet++;
         }
-        if(isspace(convertido[i])){
+        if(isspace(convertido[i])){ // conteo de espacios
           contSpaces++;
         }
-        if(convertido[i] == 33){
+        if(convertido[i] == 33){ // conteo de signos de admiracion
           contWarn++;
         }
-        if (isupper(convertido[i])) {
+        if (isupper(convertido[i])) { //conteo de mayusculas
           contCapital++;
         }
       }
       cout << "La linea " << lineCount << " contiene " << contLowerA << " letras mayúscula, " << contLowerA << " a's, " << contWarn << " ! ," << contLet << " letras, " << contSpaces << " espacios.\n\n";
     }
-    archivo.close();// se cierra el archivo
   }
   archivo.close(); // se cierra el archivo
 }
